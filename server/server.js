5d0a8e066bdee3e30a32101963e738f663f97c46
Ella <rafaela.morato1805@gmail.com>
2021-11-12 23:29:30 +1000

    fixing server folder and resolvers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const cors = require("cors");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
+const { graphqlUploadExpress } = require("graphql-upload");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -26,9 +30,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
 
 db.once("open", () => {
   app.listen(PORT, () => {
